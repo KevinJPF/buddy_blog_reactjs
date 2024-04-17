427a065836fb07ff8663aef6fc2e3d54a627ede2
KevinJPF <kevinjuliano.pires@gmail.com>
2024-04-17 17:57:46 -0300

    feat: firebase db data insertion

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,28 @@
 // CSS
+import { Link } from "react-router-dom";
 import styles from "./About.module.css";
+import { useAuthValue } from "../../context/AuthContext";
 
 const About = () => {
+  const { user } = useAuthValue();
   return (
-    <div className="About">
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h1>
+        Sobre o Mini <span>Blog</span>
+      </h1>
+      <p>
+        Esse projeto consiste em um blog feito com React no front-end e Firebase
+        no back-end.
+      </p>
+      {user ? (
+        <Link to={"/posts/create"} className="btn">
+          Criar Post
+        </Link>
+      ) : (
+        <Link to={"/login"} className="btn">
+          Entrar
+        </Link>
+      )}
     </div>
   );
 };
---
+++ src/pages/CreatePost/CreatePost.jsx
@@ -0,0 +1,102 @@
+import styles from "./CreatePost.module.css";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
+const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const { user } = useAuthValue();
+
+  const { insertDocument, response } = useInsertDocument("posts");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    // validate image URL
+
+    // create tags array
+
+    // check all values
+
+    // insert values
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+  };
+
+  return (
+    <div className={styles.create_post}>
+      <h1>Criar Post</h1>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento.</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que representa o seu post."
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por vírgula."
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+          />
+        </label>
+        {!response.loading && <button className="btn">Postar</button>}
+        {response.loading && (
+          <button disabled className="btn">
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
