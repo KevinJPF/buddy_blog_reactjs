d90b27b74e10f6d08f9534e84e65f07b043f178c
KevinJPF <kevinjuliano.pires@gmail.com>
2024-04-17 12:55:37 -0300

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.main_content}>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
