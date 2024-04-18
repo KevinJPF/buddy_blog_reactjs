1b78bb04bd5daf876f50e47f812246f8870549e6
KevinJPF <kevinjuliano.pires@gmail.com>
2024-04-18 15:12:24 -0300

    feat: create, list and see post details

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -6,20 +6,24 @@ const PostDetail = ({ post }) => {
   return (
     <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
-      <h2>{post.title}</h2>
+      <Link to={`/posts/${post.id}`}>
+        <h2>{post.title}</h2>
+      </Link>
       <p className={styles.createdBy}>
         <span>@</span>
         {post.createdBy}
       </p>
       <div className={styles.tags}>
         {post.tagsArray.map((tag) => (
-          <p key={tag}>
-            <span>#</span>
-            {tag}
-          </p>
+          <Link to={`/search?q=${tag}`} className={styles.tags}>
+            <p key={tag}>
+              <span>#</span>
+              {tag}
+            </p>
+          </Link>
         ))}
       </div>
-      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+      <Link to={`/posts/${post.id}`} className="btn btn-outline btn_read">
         Ler
       </Link>
     </div>
--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -3,13 +3,41 @@ import styles from "./Post.module.css";
 
 // hooks
 import { useParams } from "react-router-dom";
+import { useFetchDocument } from "../../hooks/useFetchDocument";
+import { Link } from "react-router-dom";
 
 const Post = () => {
   const { id } = useParams();
+  const { document: post, loading } = useFetchDocument("posts", id);
 
   return (
-    <div>
-      <h1>Post {id}</h1>
+    <div className={styles.post_container}>
+      {loading && <p>Carregando post...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Tags do post:</h3>
+          <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+              <Link to={`/search?q=${tag}`} className={styles.tags}>
+                <p key={tag}>
+                  <span>#</span>
+                  {tag}
+                </p>
+              </Link>
+            ))}
+          </div>
+          <p className={styles.createdBy}>
+            <span className="author">Autor: </span>
+            <>
+              <span>@</span>
+              {post.createdBy}
+            </>
+          </p>
+        </>
+      )}
     </div>
   );
 };
